78fbbbaa6b67ca109bdf100400b1f051df42dc34
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-17 10:16:07 +0500

    f

--- While04.py
+++ While04.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+
+    t=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            t+=1
+        i+=1 
+    return t
+print(main("CodeschoolUz"))
