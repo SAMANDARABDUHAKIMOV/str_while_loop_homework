b1c63fdd246cc7c172009098c26476ca3ee4e5cb
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-17 10:20:06 +0500

    c

--- While05.py
+++ While05.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    t=0
+    i=0
+    while i<len(s):
+        if s[i].islower():
+            t+=1
+        i+=1
+    return t
+print(main("CodeschoolUz"))
